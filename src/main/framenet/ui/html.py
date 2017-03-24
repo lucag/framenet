2163d867a5a85fe0a50263dbccdba3e39003ed14
Luca Gilardi <lucag@icsi.berkeley.edu>
2017-03-24 07:43:00 -0700

    Added collapsible sentences to pattern display

--- src/main/framenet/ui/html.py
+++ src/main/framenet/ui/html.py
@@ -1,5 +1,6 @@
 """Some very simple HTML stuff."""
 import string
+import uuid
 from numbers import Number
 from random import randrange, randint, choices
 from textwrap import dedent
@@ -29,7 +30,7 @@ def tag(elem):
             return f'<{elem}>{mkstr(ts)}</{elem}>'
     return e
 
-table, tr, td, th, b, ul, li, div = map(tag, 'table tr td th b ul li div'.split())
+table, tr, td, th, b, ul, li, div, a = map(tag, 'table tr td th b ul li div a'.split())
 
 
 def make_table_from_counts(cs, style):
@@ -63,6 +64,9 @@ data_template = dedent("""
         <div class="popover-content">
     </div>""")
 
+transitions = dedent("""
+    <script> """)
+
 r_arrow = ' \u2192 '
 
 toggle = dedent("""
@@ -88,9 +92,21 @@ css = dedent("""
         display: inherit;
     }""")
 
+link_style = dedent("""
+    <style type="text/css" media="screen">
+    a.link-hover:link { color: #000; text-decoration: none; }
+    a.link-hover:visited { color: #000; text-decoration: none; }
+    a.link-hover:hover { text-decoration: underline; } 
+    a.link-hover:active { color: #000; text-decoration: none; } 
+    </style>""")
+
 def mk_id(k=9):
     """Make a random id of length `k`."""
     return ''.join(choices(string.ascii_lowercase) + choices(string.digits + string.ascii_lowercase, k = k - 1))
+
+def uuids():
+    while True:
+        yield str(uuid.uuid4())
 
 def make_table_with_sentences(pattern_and_ss, total_count, style=align_r, include_sentences=True):
     """Table from (`pattern`, `sentence list`) pairs."""
@@ -99,13 +115,24 @@ def make_table_with_sentences(pattern_and_ss, total_count, style=align_r, includ
         return ul(li(s) for s in ss)
 
     header = tr(map(th, ('', 'freq.', 'Patterns')))
-    rows = flatten((tr(td(i + 1,   style=style),
-                       td(len(ss), style=style),
-                       td(r_arrow.join(pattern), style='vertical-align: top ; white-space: nowrap')),
-                    tr(td(slist(ss), colspan='3'), id='', Class='drawer__toggle', style='display: none'))
-                   for i, (pattern, ss) in enumerate(pattern_and_ss))
-    return (activate_popover.render()
-            + div(f'Total count: {total_count}')
-            + toggle
-            + table(header + '\n'.join(rows)))
+    rows = (tr(td(i + 1,   style=style),
+               td(len(ss), style=style),
+               td((a(r_arrow.join(pattern), **{'class': 'link-hover',
+                                               'data-toggle': 'collapse',
+                                               'href': f'#{_uuid}',
+                                               # 'data-target': _uuid
+               }),
+                   div(slist(ss), **{'id': _uuid,
+                                     'class': 'collapse',
+                                     # 'style': 'vertical-align: top ; white-space: nowrap'
+                   }))))
+            for i, (_uuid, (pattern, ss)) in enumerate(zip(uuids(), pattern_and_ss)))
+    # return (#activate_popover.render()
+    # # +
+    # div(f'Total count: {total_count}')
+    # # + toggle
+    # + table(header + '\n'.join(rows)))
+    return link_style + table(header + '\n'.join(rows)
+                              # , Class='table-hover'
+                              )
 
